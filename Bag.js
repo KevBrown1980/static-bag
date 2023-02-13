c0a62d71316463f526e957d01b539f305ea94008
KevBrown1980 <kevinpatonbrown@gmail.com>
2023-02-13 21:51:48 +0000

    3 out of 4 test working - prog works on playground - not sure why getting 1 failed test

--- Bag.js
+++ Bag.js
@@ -3,8 +3,18 @@ class Bag {
         if(!weight){
             throw new Error('bag needs weight')
         } 
+        else if (weight > 23){
+            throw new Error('bag too heavy')
+        }
         this.weight = weight
     }
-}
+
+    
+    }
+
+
+ 
+  
+
 
 module.exports = Bag
--- Bag.test.js
+++ Bag.test.js
@@ -3,11 +3,26 @@ const Bag = require('./Bag')
 describe('Bag class', () => {
     //bag has weight
     test('bag has weight', () => {
-        const bag = new Bag(25)
-        expect(bag.weight).toBe(25)
+        const bag = new Bag(20)
+        expect(bag.weight).toBe(20)
     })
     //if bag has no weight, return error
     test('bag has no weight to return an error', () => {
         expect(() => new Bag()).toThrowError('bag needs weight')
     })
+
+    //if bag is too heavy - i.e weight over 23
+    test('bag is over  23 kg', () => {
+        const bag1 = new Bag(26)
+        //expect(bag1.isOverWeight()).toBe(26)
+        expect(() => new Bag()).toThrowError('bag too heavy')
+    })
+
+     //if bag is under max weight of 23
+    test('bag is under  23 kg', () => {
+        const bag2 = new Bag(18)
+        expect(bag2.weight).toBe(18)
+    })
+  
+
 })
